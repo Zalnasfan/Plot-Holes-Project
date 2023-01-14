ac09c66cd5053ef394554c664e25cc2a27a43090
Zalnasfan <70117391+Zalnasfan@users.noreply.github.com>
2023-01-14 19:24:03 +0300

    Add files via upload

---
+++ Predictor.py
@@ -0,0 +1,52 @@
+import numpy as np
+import cv2
+import glob
+from keras.models import Sequential
+from keras.models import load_model
+from keras.utils import np_utils
+
+global size
+size = 100
+model = Sequential()
+model = load_model('D:/projects/plot_holes/sample.h5')
+
+# load Testing data : non-pothole
+nonPotholeTestImages = glob.glob(
+    "D:/projects/plot_holes/dataset/test/Plain/*.jpg")
+test2 = [cv2.imread(img, 0) for img in nonPotholeTestImages]
+for i in range(0, len(test2)):
+    test2[i] = cv2.resize(test2[i], (size, size))
+temp4 = np.asarray(test2)
+
+
+# load Testing data : potholes
+potholeTestImages = glob.glob(
+    "D:/projects/plot_holes/dataset/test/Pothole/*.jpg")
+test1 = [cv2.imread(img, 0) for img in potholeTestImages]
+for i in range(0, len(test1)):
+    test1[i] = cv2.resize(test1[i], (size, size))
+temp3 = np.asarray(test1)
+
+
+X_test = []
+X_test.extend(temp3)
+X_test.extend(temp4)
+X_test = np.asarray(X_test)
+
+X_test = X_test.reshape(X_test.shape[0], size, size, 1)
+
+
+y_test1 = np.ones([temp3.shape[0]], dtype=int)
+y_test2 = np.zeros([temp4.shape[0]], dtype=int)
+
+y_test = []
+y_test.extend(y_test1)
+y_test.extend(y_test2)
+y_test = np.asarray(y_test)
+
+y_test = np_utils.to_categorical(y_test)
+
+
+tests = model.predict(X_test)
+for i in range(len(X_test)):
+    print(">>> Predicted=%s" % (tests[i]))
